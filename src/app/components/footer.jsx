d5bc3d2cffa0c6e8cb4ac5fc2d39931c64728e48
Ragul-ZUFIZ <ragul@zufiz.com>
2025-09-06 04:28:01 +0000

    UI Modified

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -3,8 +3,8 @@
 "use client";
 
 import Link from 'next/link';
-import { CgGitFork } from "react-icons/cg";
-import { IoStar } from "react-icons/io5";
+// import { CgGitFork } from "react-icons/cg";
+// import { IoStar } from "react-icons/io5";
 
 function Footer() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ScrollToTop from "./components/helper/scroll-to-top";
+import Navbar from "./components/navbar";
+import { ToastContainer } from "react-toastify";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ToastContainer />
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+          <Navbar />
+          {children}
+          <ScrollToTop />
+        </main>
+        <Footer />
       </body>
     </html>
   );
